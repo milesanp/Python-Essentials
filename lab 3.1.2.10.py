6e7f2c62a43f0210cc6a2dbe3acd61e8373f67d1
Petru Marius Milesan <64754414+milesanp@users.noreply.github.com>
2020-05-04 01:41:36 +0100

    Add files via upload

---
+++ lab 3.1.2.10.py
@@ -0,0 +1,21 @@
+userWord = input("Welcome to a new world!\nPlease enter your word:")
+userWord = userWord.upper()
+
+#letter in range (A,E,I,O,U)
+
+for letter in userWord:
+    if letter == "A" :
+        continue
+    elif letter == "E" :
+        continue
+    elif letter == "I" :
+        continue
+    elif letter == "O" :
+        continue
+    elif letter == "U":
+        continue
+    else :
+        print(letter)
+    
+    # Complete the body of the for loop.
+    

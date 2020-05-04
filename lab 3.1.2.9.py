80d9d95e00772deb12e970069044701fd84a613e
Petru Marius Milesan <64754414+milesanp@users.noreply.github.com>
2020-05-04 01:11:09 +0100

    Add files via upload

---
+++ lab 3.1.2.9.py
@@ -0,0 +1,13 @@
+﻿cw = "chupacabra"
+
+while True :
+    word = input("Please insert your word:")
+    
+    if word == "chupacabra" :
+        break
+if word == cw :
+    print("Huuuray!!!",cw,"is the correct word.\nYou've successfully left the loop.")
+else:
+    print(input("Please insert your word:"))
+
+    
